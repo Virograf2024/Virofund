c6866225dcd7b819c04ddc62e9115595360068fa
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2025-09-12 00:48:09 +0100

    fixed firebase issues

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDIIYFwsYg-HaZBcSJ50q7GGFDiRyAj0s0",
-  authDomain: "virofund-9decf.firebaseapp.com",
-  projectId: "virofund-9decf",
-  storageBucket: "virofund-9decf.firebasestorage.app",
-  messagingSenderId: "957883740913",
-  appId: "1:957883740913:web:e087af32d5d7f3b55f2f50",
-  measurementId: "G-NLTY8XZJ4R",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
