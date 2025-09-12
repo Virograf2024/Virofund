56cb946946115d87503044a74860938ecf477d14
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2025-09-12 01:04:24 +0100

    fix: fixed syntax error in firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -22,4 +22,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const db = getFirestore(app);
-export { analytics, db };
+export { db };
