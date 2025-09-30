5d517c7abc5da005dfea6ed1b893eb604d9a17c7
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2025-09-30 23:05:20 +0100

    fix: fixed number issues

--- src/app/total-users-onboarded/page.tsx
+++ src/app/total-users-onboarded/page.tsx
@@ -1,7 +1,19 @@
+"use client";
 import { fetchWaitlist } from "@/lib/auth";
+import { useState, useEffect } from "react";
 
-export default async function TotalUsers() {
-  const data = await fetchWaitlist();
-  console.log(data.length);
-  return <div>{data.length}</div>;
+export default function TotalUsers() {
+  const [list, setList] = useState<
+    { id: string; name: string; email: string; phone: string }[]
+  >([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchWaitlist();
+      setList(data);
+    };
+    fetchData();
+  }, []);
+
+  return <div>{list.length}</div>;
 }
